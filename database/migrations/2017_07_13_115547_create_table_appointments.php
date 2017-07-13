96100a25e276a4c05e942eab0d93be4abd4542f8
Marcel <marcel_saj@hotmail.com>
2017-07-13 14:13:13 +0200

    no message

---
+++ database/migrations/2017_07_13_115547_create_table_appointments.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAppointments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient_appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('patient_id');
+            $table->integer('specialist_id');
+            $table->date('date');
+            $table->time('time');
+            $table->time('duration');
+            $table->string('treatment_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient_appointments');
+    }
+}
---
+++ database/migrations/2017_07_13_120428_create_table_patients.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePatients extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->string('adress');
+            $table->string('zipcode');
+            $table->integer('phone_number_1');
+            $table->integer('phone_number_2');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
