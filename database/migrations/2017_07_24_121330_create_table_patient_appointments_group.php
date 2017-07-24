9b338a3b0ff5273f1ab66fd754fe2d43de8fa98e
DESKTOP-AL9S59U\toine <toine.k21@gmail.com>
2017-07-24 14:35:52 +0200

    -Migrate file gecorrigeerd

--- database/migrations/2017_07_24_121330_create_table_patient_appointments_group.php
+++ database/migrations/2017_07_24_121330_create_table_patient_appointments_group.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PatientAppointmentsGroup extends Migration
+class CreateTablePatientAppointmentsGroup extends Migration
 {
     /**
      * Run the migrations.
