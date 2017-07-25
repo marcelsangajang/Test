43ef507144b9e2ccfdf80d538ae81d440fbf0540
Marcel <marcel_saj@hotmail.com>
2017-07-25 10:58:59 +0200

    no message

--- resources/views/AgendaPersonalPeriodFormView.blade.php
+++ resources/views/AgendaPersonalPeriodFormView.blade.php
@@ -43,108 +43,100 @@
     
     <h3>Monday</h3>
     <input name="start_time_mon" type="time" value="09:00:00" step="1">
+    Break 1 <input name="break1_start_mon" type="time" value="12:00:00" step="1">
+    to
+    <input name="break1_end_mon" type="time" value="12:30:00" step="1">
 
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break1_start_mon', ['11:00:00', '12:00:00', '13:00:00'], 1) }} 
-    to 
-    {{ Form::select('break1_end_mon', ['11:30:00', '12:30:00', '13:30:00'], 1) }} 
-    
     <br>
     <input name="end_time_mon" type="time" value="17:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break2_start_mon', ['14:00:00', '15:00:00', '16:00:00'], 1) }}
+    Break 2 
+    <input name="break2_start_mon" type="time" value="15:00:00" step="1">
     to
-    {{ Form::select('break2_end_mon', ['14:30:00', '15:30:00', '16:30:00'], 1) }}
+    <input name="break2_end_mon" type="time" value="15:30:00" step="1">
     <br>
     
     <h3>Tuesday</h3>
-    <input name="start_time_tue" type="time" value="09:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break1_start_tue', ['11:00:00', '12:00:00', '13:00:00'], 1) }}
-        to 
-    {{ Form::select('break1_end_tue', ['11:30:00', '12:30:00', '13:30:00'], 1) }} 
-    
+        <input name="start_time_tue" type="time" value="09:00:00" step="1">
+    Break 1 <input name="break1_start_tue" type="time" value="12:00:00" step="1">
+    to
+    <input name="break1_end_tue" type="time" value="12:30:00" step="1">
+
     <br>
     <input name="end_time_tue" type="time" value="17:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break2_start_tue', ['14:00:00', '15:00:00', '16:00:00'], 1) }}
+    Break 2 
+    <input name="break2_start_tue" type="time" value="15:00:00" step="1">
     to
-    {{ Form::select('break2_end_tue', ['14:30:00', '15:30:00', '16:30:00'], 1) }}
+    <input name="break2_end_tue" type="time" value="15:30:00" step="1">
     <br>
 
     <h3>Wednesday</h3>
-    <input name="start_time_wed" type="time" value="09:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break1_start_wed', ['11:00:00', '12:00:00', '13:00:00'], 1) }}
-        to 
-    {{ Form::select('break1_end_wed', ['11:30:00', '12:30:00', '13:30:00'], 1) }} 
-    
+        <input name="start_time_wed" type="time" value="09:00:00" step="1">
+    Break 1 <input name="break1_start_wed" type="time" value="12:00:00" step="1">
+    to
+    <input name="break1_end_wed" type="time" value="12:30:00" step="1">
+
     <br>
     <input name="end_time_wed" type="time" value="17:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break2_start_wed', ['14:00:00', '15:00:00', '16:00:00'], 1) }}
+    Break 2 
+    <input name="break2_start_wed" type="time" value="15:00:00" step="1">
     to
-    {{ Form::select('break2_end_wed', ['14:30:00', '15:30:00', '16:30:00'], 1) }}
+    <input name="break2_end_wed" type="time" value="15:30:00" step="1">
     <br>
 
     <h3>Thursday</h3>
     <input name="start_time_thu" type="time" value="09:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break1_start_thu', ['11:00:00', '12:00:00', '13:00:00'], 1) }}
-        to 
-    {{ Form::select('break1_end_thu', ['11:30:00', '12:30:00', '13:30:00'], 1) }} 
-    
+    Break 1 <input name="break1_start_thu" type="time" value="12:00:00" step="1">
+    to
+    <input name="break1_end_thu" type="time" value="12:30:00" step="1">
+
     <br>
     <input name="end_time_thu" type="time" value="17:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break2_start_thu', ['14:00:00', '15:00:00', '16:00:00'], 1) }}
+    Break 2 
+    <input name="break2_start_thu" type="time" value="15:00:00" step="1">
     to
-    {{ Form::select('break2_end_thu', ['14:30:00', '15:30:00', '16:30:00'], 1) }}
+    <input name="break2_end_thu" type="time" value="15:30:00" step="1">
     <br>
 
     <h3>Friday</h3>
     <input name="start_time_fri" type="time" value="09:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break1_start_fri', ['11:00:00', '12:00:00', '13:00:00'], 1) }}
-        to 
-    {{ Form::select('break1_end_fri', ['11:30:00', '12:30:00', '13:30:00'], 1) }} 
-    
+    Break 1 <input name="break1_start_fri" type="time" value="12:00:00" step="1">
+    to
+    <input name="break1_end_fri" type="time" value="12:30:00" step="1">
+
     <br>
     <input name="end_time_fri" type="time" value="17:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break2_start_fri', ['14:00:00', '15:00:00', '16:00:00'], 1) }}
+    Break 2 
+    <input name="break2_start_fri" type="time" value="15:00:00" step="1">
     to
-    {{ Form::select('break2_end_fri', ['14:30:00', '15:30:00', '16:30:00'], 1) }}
+    <input name="break2_end_fri" type="time" value="15:30:00" step="1">
     <br>
 
     <h3>Saturday</h3>
     <input name="start_time_sat" type="time" value="09:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break1_start_sat', ['11:00:00', '12:00:00', '13:00:00'], 1) }}
-        to 
-    {{ Form::select('break1_end_sat', ['11:30:00', '12:30:00', '13:30:00'], 1) }} 
-    
+    Break 1 <input name="break1_start_sat" type="time" value="12:00:00" step="1">
+    to
+    <input name="break1_end_sat" type="time" value="12:30:00" step="1">
+
     <br>
     <input name="end_time_sat" type="time" value="17:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break2_start_sat', ['14:00:00', '15:00:00', '16:00:00'], 1) }}
+    Break 2 
+    <input name="break2_start_sat" type="time" value="15:00:00" step="1">
     to
-    {{ Form::select('break2_end_sat', ['14:30:00', '15:30:00', '16:30:00'], 1) }}
+    <input name="break2_end_sat" type="time" value="15:30:00" step="1">
     <br>
 
     <h3>Sunday</h3>
     <input name="start_time_sun" type="time" value="09:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break1_start_sun', ['11:00:00', '12:00:00', '13:00:00'], 1) }}
-        to 
-    {{ Form::select('break1_end_sun', ['11:30:00', '12:30:00', '13:30:00'], 1) }} 
-    
+    Break 1 <input name="break1_start_sun" type="time" value="12:00:00" step="1">
+    to
+    <input name="break1_end_sun" type="time" value="12:30:00" step="1">
+
     <br>
     <input name="end_time_sun" type="time" value="17:00:00" step="1">
-    {{ Form::label('', 'Breaks') }}
-    {{ Form::select('break2_start_sun', ['14:00:00', '15:00:00', '16:00:00'], 1) }}
+    Break 2 
+    <input name="break2_start_sun" type="time" value="15:00:00" step="1">
     to
-    {{ Form::select('break2_end_sun', ['14:30:00', '15:30:00', '16:30:00'], 1) }}
+    <input name="break2_end_sun" type="time" value="15:30:00" step="1">
     <br>
     {{ Form::submit('Submit') }}
 
